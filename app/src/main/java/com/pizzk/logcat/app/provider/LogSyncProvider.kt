d709ad6c7cef35aa2d3a80a8725650109781e139
fengpeng <726676435@qq.com>
2021-09-14 14:37:39 +0800

    local test

--- app/src/main/java/com/pizzk/logcat/app/provider/LogSyncProvider.kt
+++ app/src/main/java/com/pizzk/logcat/app/provider/LogSyncProvider.kt
@@ -6,13 +6,21 @@ import com.pizzk.logcat.state.Plan
 import java.io.File
 
 class LogSyncProvider : SyncProvider() {
+    private val plan = Plan()
+    private var init = false
 
     override fun fetch(params: String): Plan? {
         Log.d("LogSyncProvider", "params: $params")
-        return super.fetch(params)
+        if (init) return null
+        init = true
+        plan.loggable = true
+        plan.reportable = true
+        plan.reportWhileWifi = true
+        return plan
     }
 
     override fun upload(file: File) {
         super.upload(file)
+        file.renameTo(File("${file.absolutePath}.bak"))
     }
 }
